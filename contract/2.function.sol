166b2065b6d075f96d08ed56d9b65af0a7ab3165
流浪少年 <3118600260@qq.com>
2026-01-07 16:45:25 +0800

    '基础学习'

---
+++ contract/2.function.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0.0;
+
+
+// 函数
+contract Helloworld{
+    string strVar = "hello world";
+    
+    // 调用方式 
+    // external  外部调用 + 导出使用
+    // public    权限最大
+    // private   内部使用
+    // internal  内部调用 + 继承/派生使用
+    
+    function sayHell()public{
+
+    }
+}
+
+
