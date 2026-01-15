6c53551cdccf993708173b7cab82ec9f4def831e
流浪少年 <3118600260@qq.com>
2026-01-15 11:31:49 +0800

    优化重入攻击，去重

--- ChainlinkWeb3Learn/3.FundMe/FundMe.sol
+++ ChainlinkWeb3Learn/3.FundMe/FundMe.sol
@@ -11,6 +11,8 @@ import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/Ag
 contract FundMe {
     // 投资人和投资数量
     mapping(address => uint256) public fundersToAmount;
+    // 无法使用mapping 遍历，使用数组记录
+    address[] public funders;
 
     // 内部使用
     AggregatorV3Interface internal dataFeed;
@@ -35,8 +37,13 @@ contract FundMe {
             covertEthToUsd(msg.value) >= MIN_VALUE,
             "error Sender more Eth(1ETH)"
         );
+
+        if (fundersToAmount[msg.sender] == 0) {
+            funders.push(msg.sender);
+        }
+
         // 添加操作
-        fundersToAmount[msg.sender] = fundersToAmount[msg.sender]+msg.value;
+        fundersToAmount[msg.sender] += msg.value;
     }
 
     function getChainlinkDataFeedLatestAnswer() public view returns (int) {
@@ -59,41 +66,60 @@ contract FundMe {
         // x /eth = 10*18
     }
 
-    function getFund() public{
+    ///1. transfer 纯转账 ， 失败会回滚
+    // payable(msg.sender).transfer(address(this).balance);
+
+    // send     纯转账 返回是否成功
+    // 2. bool success = payable(msg.sender).send(address(this).balance);
+    // require(success);
+
+    // call  带数据,可以调用其他 payable 的函数,推荐使用
+    // 3. (bool,result) = addr.call{value,value}("sendeDate");
+
+    function getFund() public {
+        // 1.先这个，可以优化gas
+        require(msg.sender == ower, "not owner");
+
+        // 这个检查相对比较复杂
         // this 表示当前合约
         // address 获取地址，balance 获取余额
         require(
             covertEthToUsd(address(this).balance) >= TARGET_VALUE,
             "TargetValue is not enough"
         );
-        require(msg.sender == ower, "not owner");
-        ///1. transfer 纯转账 ， 失败会回滚
-        // payable(msg.sender).transfer(address(this).balance);
 
-        // send     纯转账 返回是否成功
-        // 2. bool success = payable(msg.sender).send(address(this).balance);
-        // require(success);
+        for (uint256 i = 0; i < funders.length; i++) {
+            address funder = funders[i]; // 获取数组
+            fundersToAmount[funder] = 0; // 状态清零
+        }
 
-        // call  带数据,可以调用其他 payable 的函数,推荐使用
-        // 3. (bool,result) = addr.call{value,value}("sendeDate");
+        funders = new address[](0); // 清空数组
+
         bool result;
-        (result,) = payable(msg.sender).call{value:address(this).balance}("");
-        require(result,"transfer tx failed");
-        
+        (result, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(result, "transfer tx failed");
     }
+
     // 改变所有者
     function transferOwnerShip(address newOwer) public {
         require(ower == msg.sender, "not owner");
         ower = newOwer;
     }
 
-    function reFund()public{
-        require(covertEthToUsd(address(this).balance)<TARGET_VALUE,"Target is reached"); // 筹集到了对应的资产
-        require(fundersToAmount[msg.sender]>0,"Not have memory");
-        bool success;
-        (success,) =payable(msg.sender).call{value:fundersToAmount[msg.sender]}("reFund success");
-        require(success,"transfer tx failed");
-        // 去重，清零
+    function reFund() public {
+        require(
+            covertEthToUsd(address(this).balance) < TARGET_VALUE,
+            "Target is reached"
+        ); // 筹集到了对应的资产
+        require(fundersToAmount[msg.sender] > 0, "Not have memory");
+
+        // 去重，清零,防止重入攻击
         fundersToAmount[msg.sender] = 0;
+
+        bool success;
+        (success, ) = payable(msg.sender).call{
+            value: fundersToAmount[msg.sender]
+        }("reFund success");
+        require(success, "transfer tx failed");
     }
 }
